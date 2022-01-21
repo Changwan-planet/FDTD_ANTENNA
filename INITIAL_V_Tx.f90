e959c94b8ac100f4453fd356f2ff4156ef3612c6
Changwan <tjsckddhks@gmail.com>
2022-01-21 09:54:24 +0900

    update for antenna

--- INITIAL_V_Tx.f90
+++ INITIAL_V_Tx.f90
@@ -27,6 +27,56 @@ DS = 4.0 * c * DT
 !++++++COEFFECIENT OF 1D WAVE EQUATION++++++
 !     +++++++++++++++++++++++++++++++++
 
+CT_S = C * DT/DS
+
+!     ++++++++++++++++++
+!++++++SOURCE_FREQUENCY++++++
+!     ++++++++++++++++++
+F_SOURCE = 6.0+9
+
+!     ++++++++++++++
+!++++++SOURCE POINT++++++
+!     ++++++++++++++
+K_SOURCE = 100
+
+!     +++++++++++++++++++++
+!++++++END (FEEDING) POINT++++++
+!     +++++++++++++++++++++
+K_END = 200
+
+!     +++++++++++++++++++++++++++++++++
+!++++++FEEDING POINT IN 3D LATICE : Tx++++++
+!     +++++++++++++++++++++++++++++++++
+i_Tx_ANT = Nx/2 - (20)
+j_Tx_ANT = Ny/2
+k_Tx_ANT = 112 
+
+
+!     +++++++++++++++++++++++++++++++++
+!++++++FEEDING POINT IN #D LATICE : Rx++++++
+!     +++++++++++++++++++++++++++++++++
+i_Tx_ANT(0) = Nx/2 + (20)
+j_Tx_ANT(0) = j_Tx_ANT
+k_Tx_ANT(0) = k_Tx_ANT
+
+i_Tx_ANT(1) = Nx/2 - (60)
+j_Tx_ANT(1) = j_Tx_ANT
+k_Tx_ANT(1) = k_Tx_ANT
+
+i_Tx_ANT(2) = Nx/2 + (60)
+j_Tx_ANT(2) = j_Tx_ANT
+k_Tx_ANT(2) = k_Tx_ANT
+
+i_Tx_ANT(3) = Nx/2 - (100)
+j_Tx_ANT(3) = j_Tx_ANT
+k_Tx_ANT(3) = k_Tx_ANT
+
+i_Tx_ANT(4) = Nx/2 + (100)
+j_Tx_ANT(4) = j_Tx_ANT
+k_Tx_ANT(4) = k_Tx_ANT
+
+READ
+
 
 
 RETURN
